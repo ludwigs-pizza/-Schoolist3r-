70216674a4cdde0db89ef19067b22da3c675b6e2
Noderz <58016133+Noderz@users.noreply.github.com>
2020-02-20 16:56:24 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+var url = require('url');
+var querystring = require('querystring');
+var express = require('express');
+var unblocker = require('unblocker');
+var Transform = require('stream').Transform;
+
+var app = express();
+
+var google_analytics_id = process.env.GA_ID || null;
+
+function addGa(html) {
+      if (google_analytics_id) {
+var ga = [
+  "<script async src='https://www.googletagmanager.com/gtag/js?id=UA-139761472-1'></script>",
+"<script>",
+  "window.dataLayer = window.dataLayer || [];",
+  "function gtag(){dataLayer.push(arguments);}",
+  "gtag('js', new Date());",
+  "gtag('config', 'UA-139761472-1');",
+"</script>"
+            ].join("\n");
+        html = html.replace("</body>", ga + "\n\n</body>");
+    }
+    return html;
+}
+
+function googleAnalyticsMiddleware(data) {
+    if (data.contentType == 'text/html') {
+
+        // https://nodejs.org/api/stream.html#stream_transform
+        data.stream = data.stream.pipe(new Transform({
+            decodeStrings: false,
+            transform: function(chunk, encoding, next) {
+                this.push(addGa(chunk.toString()));
+                next();
+            }
+        }));
+    }
+}
+
+var unblockerConfig = {
+    prefix: '/suibndiuvhdifuhvisduhvisduvhisduhvisduhvi/',
+    responseMiddleware: [
+        googleAnalyticsMiddleware
+    ]
+};
+
+
+
+// this line must appear before any express.static calls (or anything else that sends responses)
+app.use(unblocker(unblockerConfig));
+
+// serve up static files *after* the proxy is run
+app.use('/', express.static(__dirname + '/public'));
+
+// this is for users who's form actually submitted due to JS being disabled or whatever
+app.get("/no-js", function(req, res) {
+    // grab the "url" parameter from the querystring
+    var site = querystring.parse(url.parse(req.url).query).url;
+    // and redirect the user to /list/url
+    res.redirect(unblockerConfig.prefix + site);
+});
+
+// for compatibility with gatlin and other servers, export the app rather than passing it directly to http.createServer
+module.exports = app;
