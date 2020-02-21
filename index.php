4074a3ea2ba9d23c5050466647d8cc4d161a05ef
Noderz <58016133+Noderz@users.noreply.github.com>
2020-02-21 13:34:48 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>My Drive - Google Drive</title>
+<link rel="icon" href="https://raw.githubusercontent.com/Noderz/schoolister/master/download%20(3).ico">
+</head>
+<body style="background-color:powderblue;">
+<script src="js/main.js"></script>
+<center>
+<h1 style="font-family:comic sans ms;background-color:tomato"><ins>Schoolist3r</ins></h1>
+<br>
+<img src="nlogo.png" width = 300 height= 300 alt= logo>
+</br>
+<br>
+<ins>
+<i>
+<h2 style="font-family:Agency FB;">Made For Students, By Students.</h2>
+</i>
+</ins>
+<p style="font-family:Calibri;background-color:tomato"> Before using, make sure to put your browser in private/incognito mode. <ins>If you do not put in that mode, the site will be blocked.</ins> 
+<br> Please do me a favor on doing that. It really helps me keep this site running. Thank you! -Noderz
+<br>
+<br>
+<b><i><u>Last Updated 2/9/20. Replaced Kahoot & Quizizz Flooder Link, Fixed Paper.io, Updated Tools, Updated Arcade, Added NodeCookie To Surf Freely, And Finally Fixed CoolMathGames Servers.</u></i></b>
+</p>
+</style>
+</br>
+</body>
+</br>
+<a href="games.html"><button style="color:white;font-family:comic sans ms;background-color:DodgerBlue;height:200px;width:300px"><ins>Arcade</ins></button></a><a href="proxselection.html"><button style="color:white;font-family:comic sans ms;background-color:DodgerBlue;height:200px;width:300px"><ins>Surf Freely</ins></button></a><a href="tools.html"><button style="color:white;font-family:comic sans ms;background-color:DodgerBlue;height:200px;width:300px"><ins>Useful Tools</ins></button></a>
+<br>
+<img src="download.gif" width= 1000 height= 562 alt= gif />
+</br>
